b49f2bc9fc7075184ad66cb1b35c35ca9c974703
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-09-05 11:59:12 +0200

    The header is now sticky

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import CanteenSelection from "./canteen-selection/CanteenSelection";
 import DaySelection from "./day-selection/DaySelection";
 import DietSelection from "./diet-selection/DietSelection";
@@ -5,8 +6,23 @@ import DietSelection from "./diet-selection/DietSelection";
 import "./Header.css";
 
 function Header() {
+  const handleScroll = () => {
+    if(window.scrollY > 0) {
+      document.querySelector('.sticky')?.classList.add('scrolled')
+    } else {
+      document.querySelector('.sticky')?.classList.remove('scrolled')
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className="sticky">
       <div className="row">
         <h1>
           <a href="#">UUlm Mensa</a>
