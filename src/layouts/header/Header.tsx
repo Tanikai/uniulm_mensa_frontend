a545e668953de24e29657939eabebd5d86ad709f
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2024-09-06 12:56:21 +0200

    Implemented new header behaviour

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,21 +1,44 @@
 import { useEffect } from 'react';
+import { useScroll } from '../../hooks/useScroll';
 import CanteenSelection from "./canteen-selection/CanteenSelection";
 import DaySelection from "./day-selection/DaySelection";
 import DietSelection from "./diet-selection/DietSelection";
 
 import "./Header.css";
-import { useScroll } from '../../hooks/useScroll';
 
 function Header() {
-  const [scrollY, distance] = useScroll();
+  const {position, atTop, scrolledUp, scrolledDown} = useScroll();
 
-  useEffect(() => {
-    if(scrollY > 0) {
-      document.querySelector('.sticky')?.classList.add('scrolled')
-    } else {
-      document.querySelector('.sticky')?.classList.remove('scrolled')
+  useEffect(() => { // Hide header on scroll down, show on scroll up
+    const header = document.querySelector('.sticky');
+    if(header === null)
+      return;
+
+    if(scrolledDown) {
+      header.classList.add('hidden');
+    } else if(scrolledUp) {
+      header.classList.remove('hidden');
     }
-  }, [scrollY, distance]);
+  }, [scrolledUp, scrolledDown]);
+
+  useEffect(() => { // Determine, when the header should be sticky and when it should flow with the page
+    const header = document.querySelector('.sticky');
+    if(header === null)
+      return;
+
+    if(atTop) { // Header is at the top of the page -> not sticky, flow with the page
+      header.classList.add('fixed');
+    } else if(position > header.getBoundingClientRect().height) { // The header is completely scrolled off-screen -> sticky, do not flow with the page
+      const wasFixed = header.classList.contains('fixed');
+      header.classList.remove('fixed');
+      if(wasFixed) { // If the header was previously sticky, do not animate it, this would create a weird flicker/jump
+        header.getAnimations()
+          .map(anim => anim as CSSTransition)
+          .filter(anim => anim.transitionProperty === 'transform')
+          .forEach(anim => { anim.finish(); });
+      }
+    }
+  }, [atTop, position]);
 
   return (
     <header className="sticky">
