2918235fa7f0c80ac0f5ac7531f4e37da8d9e92e
Kai Anter <kai@anter.dev>
2025-03-31 15:37:30 +0200

    feat(grouping): unanimated accordion for groups

--- src/layouts/canteen-meals/meal-accordion/MealAccordion.tsx
+++ src/layouts/canteen-meals/meal-accordion/MealAccordion.tsx
@@ -1,6 +1,9 @@
 import { Meal } from "../../../providers/DataContext.tsx";
 import MealElement from "../meal-element/MealElement.tsx";
 
+import "./MealAccordion.css";
+import { useState } from "react";
+
 export interface MealAccordionProps {
   label: string;
   meals: Meal[];
@@ -12,21 +15,42 @@ export function MealAccordion({
   meals,
   onInfoClicked,
 }: MealAccordionProps) {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     // TODO: make accordion expandable
     <div className="meal-accordion">
-      <h2>{label}</h2>
-      {meals.length === 0 ? (
-        <div className="meal-element">Keine Gerichte vorhanden.</div>
-      ) : (
-        meals.map((meal) => (
-          <MealElement
-            key={meal.type}
-            meal={meal}
-            onInfoClicked={onInfoClicked}
-          />
-        ))
-      )}
+      {/*header that is always visible*/}
+      <div
+        className={"meal-accordion-header"}
+        onClick={() => {
+          toggleOpen();
+        }}
+      >
+        <h2>{label}</h2>
+        <span className={"caret"}>{open ? "⋀" : "⋁"}</span>
+      </div>
+      {/*body that is toggled*/}
+      <div
+        className={`meal-accordion-body ${open ? "meal-accordion-opened" : "meal-accordion-closed"}`}
+      >
+        {meals.length === 0 ? (
+          <div className="meal-element">Keine Gerichte vorhanden.</div>
+        ) : (
+          meals.map((meal) => (
+            <MealElement
+              key={meal.type}
+              meal={meal}
+              onInfoClicked={onInfoClicked}
+            />
+          ))
+        )}
+      </div>
+      <hr />
     </div>
   );
 }
