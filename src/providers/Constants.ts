975635712f207d1da989aee96a784cc7b0556aad
Kai Anter <kai@anter.dev>
2025-05-05 16:14:36 +0200

    fix regression with "bio" and diet filters

--- src/providers/Constants.ts
+++ src/providers/Constants.ts
@@ -22,14 +22,23 @@ export enum Diet {
 
 export const DietSets: Record<Diet, Set<MealType>> = {
   [Diet.Unrestricted]: new Set(Object.values(MealType)),
-  [Diet.Vegan]: new Set([MealType.Vegan]),
-  [Diet.Vegetarian]: new Set([MealType.Vegan, MealType.Vegetarian]),
+  [Diet.Vegan]: new Set([MealType.Vegan, MealType.Bio]),
+  [Diet.Vegetarian]: new Set([
+    MealType.Vegan,
+    MealType.Vegetarian,
+    MealType.Bio,
+  ]),
   [Diet.NoPork]: new Set(
     Object.values(MealType).filter((type) => type !== MealType.Pork),
   ),
-  [Diet.Meat]: new Set([MealType.Pork, MealType.Beef, MealType.Poultry]),
-  [Diet.Fish]: new Set([MealType.Fish]),
-  [Diet.Pork]: new Set([MealType.Pork]),
-  [Diet.Beef]: new Set([MealType.Beef]),
-  [Diet.Poultry]: new Set([MealType.Poultry]),
+  [Diet.Meat]: new Set([
+    MealType.Pork,
+    MealType.Beef,
+    MealType.Poultry,
+    MealType.Bio,
+  ]),
+  [Diet.Fish]: new Set([MealType.Fish, MealType.Bio]),
+  [Diet.Pork]: new Set([MealType.Pork, MealType.Bio]),
+  [Diet.Beef]: new Set([MealType.Beef, MealType.Bio]),
+  [Diet.Poultry]: new Set([MealType.Poultry, MealType.Bio]),
 };
